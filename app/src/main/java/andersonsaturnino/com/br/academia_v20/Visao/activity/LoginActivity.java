2e9b3cd3522f070247212f7e1fbb24e630970242
Lucas Ferreira <lucaskfm@gmail.com>
2016-03-17 12:07:26 -0300

    Alteração na tela de login

--- app/src/main/java/andersonsaturnino/com/br/academia_v20/Visao/activity/LoginActivity.java
+++ app/src/main/java/andersonsaturnino/com/br/academia_v20/Visao/activity/LoginActivity.java
@@ -1,17 +1,15 @@
 package andersonsaturnino.com.br.academia_v20.Visao.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
 
 import andersonsaturnino.com.br.academia_v20.R;
-import andersonsaturnino.com.br.academia_v20.Visao.maps.Mapa;
 
 /**
  * Classe responsavel por realizar login
@@ -29,42 +27,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_login);
-        toolbar.setTitle("Logar");
-        setSupportActionBar(toolbar);
-
-        logarFace = (Button) findViewById(R.id.logarFb);
-        logarFace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(LoginActivity.this, "Facebook", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        logarLocal = (Button) findViewById(R.id.logar);
-        logarLocal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(LoginActivity.this, "Logando", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.mnCadastrar) {
-            Intent login = new Intent(LoginActivity.this, Cadastro_Usuario.class);
-            startActivity(login);
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
