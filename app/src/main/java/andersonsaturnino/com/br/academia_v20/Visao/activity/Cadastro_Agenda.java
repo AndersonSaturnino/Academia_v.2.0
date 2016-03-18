42c096e4c7c325b861d456c972eedaf6fd91f65a
Lucas <lucaskfm@gmail.com>
2016-03-18 14:55:25 -0300

    Correção de Bugs

--- app/src/main/java/andersonsaturnino/com/br/academia_v20/Visao/activity/Cadastro_Agenda.java
+++ app/src/main/java/andersonsaturnino/com/br/academia_v20/Visao/activity/Cadastro_Agenda.java
@@ -2,12 +2,11 @@ package andersonsaturnino.com.br.academia_v20.Visao.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -64,67 +63,75 @@ public class Cadastro_Agenda extends AppCompatActivity {
         edtSerie = (EditText)findViewById(R.id.edtSerie);
         edtRepeticoes = (EditText)findViewById(R.id.edtRepeticoes);
         edtPeso = (EditText)findViewById(R.id.edtPeso);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
     }
 
-//    public void btnSalvarClick(View view) {
-//
-//        // Verifica se a Semana foi selecionado;
-//        if (cbSemana.getSelectedItemId() == 0) {
-//            Toast.makeText(this, "Selecione Um Dia Da Semana Antes de Salvar!", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//        // Verifica se o Treino foi selecionado;
-//        if (cbTreinos.getSelectedItemId() == 0) {
-//            Toast.makeText(this, "Selecione Um Treino Antes de Salvar!", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//        // Verifica se a Série foi digitada;
-//        if (edtSerie.getText().length() == 0) {
-//            Toast.makeText(this, "Informe a Série Antes de Salvar!", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//        // Verifica se a Repetição foi digitada;
-//        if (edtRepeticoes.getText().length() == 0) {
-//            Toast.makeText(this, "Informe a Repetição Antes de Salvar!", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//        // Verifica se o Peso foi digitada;
-//        if (edtPeso.getText().length() == 0) {
-//            Toast.makeText(this, "Informe o Peso Antes de Salvar!", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//        try{
-//            long codSemana = cbSemana.getSelectedItemId();
-//            semanaDAO = new SemanaDAO(this);
-//            Semanas semana =  semanaDAO.buscaPorCodigo(codSemana);
-//
-//            long codTreino= cbTreinos.getSelectedItemId();
-//            treinoDAO = new TreinoDAO(this);
-//            Treinos treinos = treinoDAO.buscaPorCodigo(codTreino);
-//
-//            agendamentoDAO = new AgendamentoDAO(this);
-//            agendamentos.setSemana(semana);
-//            agendamentos.setTreino(treinos);
-//            agendamentos.setPeso(Integer.parseInt(edtPeso.getText().toString()));
-//            agendamentos.setRepeticao(Integer.parseInt(edtRepeticoes.getText().toString()));
-//            agendamentos.setSerie(Integer.parseInt(edtSerie.getText().toString()));
-//
-//            String msg = agendamentoDAO.incluirAgendamento(agendamentos);
-//
-//            limpaCampos();
-//
-//            Snackbar.make(view, msg, Snackbar.LENGTH_LONG).setAction("Action", null).show();
-//
-//
-//        }catch (Exception e) {
-//            Log.i("ERRO", e.getMessage().toString());
-//        }
-//    }
+    public void btnSalvarClick(View view) {
+
+        // Verifica se a Semana foi selecionado;
+        if (cbSemana.getSelectedItemId() == 0) {
+            Toast.makeText(this, "Selecione Um Dia Da Semana Antes de Salvar!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Verifica se o Treino foi selecionado;
+        if (cbTreinos.getSelectedItemId() == 0) {
+            Toast.makeText(this, "Selecione Um Treino Antes de Salvar!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Verifica se a Série foi digitada;
+        if (edtSerie.getText().length() == 0) {
+            Toast.makeText(this, "Informe a Série Antes de Salvar!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Verifica se a Repetição foi digitada;
+        if (edtRepeticoes.getText().length() == 0) {
+            Toast.makeText(this, "Informe a Repetição Antes de Salvar!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Verifica se o Peso foi digitada;
+        if (edtPeso.getText().length() == 0) {
+            Toast.makeText(this, "Informe o Peso Antes de Salvar!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try{
+            long codSemana = cbSemana.getSelectedItemId();
+            semanaDAO = new SemanaDAO(this);
+            Semanas semana =  semanaDAO.buscaPorCodigo(codSemana);
+
+            long codTreino= cbTreinos.getSelectedItemId();
+            treinoDAO = new TreinoDAO(this);
+            Treinos treinos = treinoDAO.buscaPorCodigo(codTreino);
+
+            agendamentoDAO = new AgendamentoDAO(this);
+            agendamentos.setSemana(semana);
+            agendamentos.setTreino(treinos);
+            agendamentos.setPeso(Integer.parseInt(edtPeso.getText().toString()));
+            agendamentos.setRepeticao(Integer.parseInt(edtRepeticoes.getText().toString()));
+            agendamentos.setSerie(Integer.parseInt(edtSerie.getText().toString()));
+
+            String msg = agendamentoDAO.incluirAgendamento(agendamentos);
+
+            limpaCampos();
+
+            Snackbar.make(view, msg, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+
+        }catch (Exception e) {
+            Log.i("ERRO", e.getMessage().toString());
+        }
+    }
 
     public void btnListaTreinosClick(View view) {
         intent = new Intent(this, Lista_Treinos.class);
@@ -141,72 +148,5 @@ public class Cadastro_Agenda extends AppCompatActivity {
         edtSerie.setText(null);
         edtRepeticoes.setText(null);
         edtPeso.setText(null);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.mn_cadastro_agenda, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.mnSalvarAgenda) {
-            // Verifica se a Semana foi selecionado;
-            if (cbSemana.getSelectedItemId() == 0) {
-                Toast.makeText(this, "Selecione Um Dia Da Semana Antes de Salvar!", Toast.LENGTH_SHORT).show();
-            }
-
-            // Verifica se o Treino foi selecionado;
-            if (cbTreinos.getSelectedItemId() == 0) {
-                Toast.makeText(this, "Selecione Um Treino Antes de Salvar!", Toast.LENGTH_SHORT).show();
-            }
-
-            // Verifica se a Série foi digitada;
-            if (edtSerie.getText().length() == 0) {
-                Toast.makeText(this, "Informe a Série Antes de Salvar!", Toast.LENGTH_SHORT).show();
-            }
-
-            // Verifica se a Repetição foi digitada;
-            if (edtRepeticoes.getText().length() == 0) {
-                Toast.makeText(this, "Informe a Repetição Antes de Salvar!", Toast.LENGTH_SHORT).show();
-            }
-
-            // Verifica se o Peso foi digitada;
-            if (edtPeso.getText().length() == 0) {
-                Toast.makeText(this, "Informe o Peso Antes de Salvar!", Toast.LENGTH_SHORT).show();
-            }
-
-            try {
-                long codSemana = cbSemana.getSelectedItemId();
-                semanaDAO = new SemanaDAO(this);
-                Semanas semana = semanaDAO.buscaPorCodigo(codSemana);
-
-                long codTreino = cbTreinos.getSelectedItemId();
-                treinoDAO = new TreinoDAO(this);
-                Treinos treinos = treinoDAO.buscaPorCodigo(codTreino);
-
-                agendamentoDAO = new AgendamentoDAO(this);
-                agendamentos.setSemana(semana);
-                agendamentos.setTreino(treinos);
-                agendamentos.setPeso(Integer.parseInt(edtPeso.getText().toString()));
-                agendamentos.setRepeticao(Integer.parseInt(edtRepeticoes.getText().toString()));
-                agendamentos.setSerie(Integer.parseInt(edtSerie.getText().toString()));
-
-                String msg = agendamentoDAO.incluirAgendamento(agendamentos);
-
-                limpaCampos();
-
-                Toast.makeText(Cadastro_Agenda.this, "Exercicio Agendado", Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                Log.i("ERRO", e.getMessage().toString());
-                Toast.makeText(Cadastro_Agenda.this, "Erro: " + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
-            }
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/andersonsaturnino/com/br/academia_v20/Visao/activity/LoginActivity.java
+++ app/src/main/java/andersonsaturnino/com/br/academia_v20/Visao/activity/LoginActivity.java
@@ -4,11 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
 import andersonsaturnino.com.br.academia_v20.R;
@@ -28,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
